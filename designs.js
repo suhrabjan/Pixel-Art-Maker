c0c8d3e31ecdd3c40ba01d6cf6434b05da8eed74
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-08-07 01:47:27 -0500

    Update to JS file: add makeGrid func, pixelPainter func, pixelEraser func; Update to HTML file: add button files for pointer, brush, and eraser.

--- designs.js
+++ designs.js
@@ -1,10 +1,52 @@
-// Select color input
-// Select size input
+function makeGrid(evt) {
+    evt.preventDefault();
+    const table = document.querySelector('#pixelCanvas')
+    const gridHeight = parseInt(document.querySelector('#inputHeight').value)
+    const gridWidth = parseInt(document.querySelector('#inputWidth').value)
+    if (table.innerHTML !== ''){
+        table.innerHTML = '';
+    };
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+    for (let i = 0; i < gridHeight; i++) {
+        const newTrElement = document.createElement('tr');
+        newTrElement.className = 'tableRow';
+        table.appendChild(newTrElement);
+        for (let j = 0; j < gridWidth; j++) {
+            const newTdElement = document.createElement('td');
+            newTdElement.className = 'tableCell';
+            newTrElement.appendChild(newTdElement);
+        }
+    }
 }
+
+function pixelPainter(event) {
+    if (event.target.nodeName === 'TD'){
+        event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+    }
+}
+
+function pixelEraser(e) {
+    if (e.target.nodeName === 'TD') {
+        e.target.style.backgroundColor = 'white';
+    }
+}
+
+document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
+
+const theTable = document.getElementById('pixelCanvas')
+theTable.addEventListener('click', pixelPainter);
+
+document.getElementById('buttons').addEventListener('click', function(evt){
+    let target = evt.target;
+    if (target.id === 'brush') {
+        theTable.removeEventListener('click', pixelEraser)
+        theTable.addEventListener('mouseover', pixelPainter);
+    } else if (target.id === 'pointer') {
+        theTable.removeEventListener('mouseover', pixelPainter);
+        theTable.removeEventListener('click', pixelEraser)
+        theTable.addEventListener('click', pixelPainter);
+    } else if (target.id === 'eraser') {
+        theTable.removeEventListener('mouseover', pixelPainter);
+        theTable.addEventListener('click', pixelEraser)
+    }
+})
